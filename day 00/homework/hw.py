8e9b93a4aa1037fb0e8ed90844bce570b13b2713
saba1029 <sabaeliz72@gmail.com>
2025-10-04 10:36:19 +0400

    commit

--- day 00/homework/hw.py
+++ day 00/homework/hw.py
@@ -4,7 +4,7 @@ speed(30)
 width(7)
 color("purple")
 
-
+begin_fill()
 forward(200)
 left(90)
 
@@ -16,15 +16,18 @@ left(90)
 
 forward(200)
 left(90)
+end_fill()
 
 forward(70)
 color("yellow")
+begin_fill()
 left(90)
 forward(120)
 right(90)
 forward(60)
 right(90)
 forward(120)
+end_fill()
 
 penup()
 goto(200,200)
