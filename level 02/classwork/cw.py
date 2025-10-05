ddd2126481c7803d2028fa3eaaf997627f11dce0
saba1029 <sabaeliz72@gmail.com>
2025-10-05 21:13:32 +0400

    commit

---
+++ level 02/classwork/cw.py
@@ -0,0 +1,15 @@
+print("gamarjoba")
+print(100)
+print(15.6)
+
+name="saba"
+print("my name is saba")
+
+age=14
+print("i am 14 years old")
+
+fawfood="mwvadi"
+print("my fawourite food is mwvadi")
+
+fawcolor="green"
+print("my fawourite color is green")
