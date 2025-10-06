8ef885571a0a9ec6304bcc8ef908df4c42a8eb30
saba1029 <sabaeliz72@gmail.com>
2025-10-06 23:12:28 +0400

    commit

---
+++ level 02/homework/hw.py
@@ -0,0 +1,33 @@
+#string-არის ნებისმიერი კოდი რომელიც არის მოთავსებული "" შორის.
+#integer-არის ნებისმიერი მთელი რიცხვი.
+#float-არის ნებისმიერი ათწილადი.
+#variable-არის ცვლადი, ის არის მეხსიერების ადგილი, რომელსაც პროგრამა იყენებს იმფორმაციის შესანახად.
+
+print("i")
+print("am")
+print("fourteen")
+print("years")
+print("old")
+
+print(14)
+print(10)
+print(20)
+print(34)
+print(43)
+
+print(12.3)
+print(32.4)
+print(34.5)
+print(83.2)
+print(59.1)
+
+greeting="hello"
+print("hello")
+person="i"
+print("i")
+to_be="am"
+print("am")
+age=14
+print(14)
+farewell="bye"
+print("bye")
