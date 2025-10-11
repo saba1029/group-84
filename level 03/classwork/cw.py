3498784b3cd3805c72b1bc5e0f0fd6be55d3d315
saba1029 <sabaeliz72@gmail.com>
2025-10-11 13:13:32 +0400

    commit

---
+++ level 03/classwork/cw.py
@@ -0,0 +1,16 @@
+print(14 + 20)
+print(21.3 + 43.1)
+
+print(3 * 5)
+print(2.5 * 2.3)
+
+print(32 / 19)
+print(78 // 23)
+
+name="Saba"
+age="14"
+fawcolor="green"
+fawfood="mwvadi"
+fawcarbrand="Porsche"
+
+print(name, age, fawcolor, fawfood, fawcarbrand)
