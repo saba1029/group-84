07d1457e265454d3f768653f2001436b2f5ff921
saba1029 <sabaeliz72@gmail.com>
2025-10-12 11:04:58 +0400

    commit

---
+++ level 03/homework/hw.py
@@ -0,0 +1,37 @@
+# concantation --> არის 2 სტრინგის შეერთება "+" სიმბოლოს გამოყენებით
+
+num1="me"
+num2="shevasrule"
+num3="davaleba"
+print(num1, num2, num3)
+
+lenght=20
+widt=10
+P=2*(20+10)
+print(P)
+
+print(20-2)
+print(213-43)
+print(13-23)
+
+print(12+45)
+print(54+98)
+print(39+92)
+
+print(4*9)
+print(23*98)
+print(123*456)
+
+print(32/12)
+print(74/15)
+print(81/73)
+
+print(832//34)
+print(845//28)
+print(821//61)
+
+name="saba"
+age=14
+programe="python"
+
+print("I'm " + name + "I'm " +"learning " + programe)
