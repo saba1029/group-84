4162ce49764c7ee343cd89c77a21b5c7daa2fa81
saba1029 <sabaeliz72@gmail.com>
2025-10-25 09:23:48 +0400

    commit

---
+++ level 06/classwork/cw.py
@@ -0,0 +1,18 @@
+age=14
+print(age)
+
+# input --> არის მონაცემები რაც კომპიუტერში შედის.
+# output --> არის მონაცემეი რაც არის ადამიანისთვის. ეს არის როდესაც კომპიუტერი ადამიანს რამეს გადმოგვცმს.
+
+a =int(input("230"))
+b =int(input("100"))
+print(a + b)
+print(a - b)
+print(a * b)
+print(1 / b)
+print(a // b)
+
+faw_color="green"
+faw_animal="dog"
+faw_country="Georgia"
+print("my faw color is" + " " + faw_color + " " + "my faw animal is" + " " + faw_animal + " " + "my faw country is" + " " + faw_country)
---
+++ level 07/homework/hm.py
@@ -0,0 +1,23 @@
+#integer ეს არის მთელი რიცხვი. გამოიყენება, როცა გვჭირდება მთლიანი რიცხვები.
+#float ეს არის ათწილადი. გამოიყენება ზუსტი მნიშვნელობებისთვის, როცა საჭიროა წერტილის შემდეგი ციფრებიც.
+#string ეს არის ტექსტი რომელიც ბრჭყალებშია მოქცეულიგამოიყენება ტექსტის შესანახად.
+
+a = float(input(10))
+b = float(input(20))
+c = float(input(30))
+average = (a + b + c) / 3
+print("avearage is ", average)
+
+a = float(input(213))
+b = float(input(928))
+sum =  a + b
+print("ორი რიცხვის ჯამია:", sum)
+
+my_name = input("saba")
+little_sister = input("nia")
+big_sister = input("nini")
+mother_name = input("teo")
+father_name = input("giorgi")
+sentence = "my name is " + my_name + ", my big sisters name is " + big_sister +   ", my little sisters name is " + little_sister +   ", my mothers name is " + mother_name + ", my fathers name is — " + father_name + "."
+print(sentence)
+
