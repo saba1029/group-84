a6fa5f562e22d4f377c33a886e94a3f15bcbaf8a
saba1029 <sabaeliz72@gmail.com>
2025-11-02 21:02:33 +0400

    commit

---
+++ level 10/classwork/cw.py
@@ -0,0 +1,18 @@
+# 1)
+print(10 > 5)  # true
+print(3 > 8)   # false
+
+print(2 < 9)   # true
+print(10 < 4)  # false
+
+print(5 <= 5)  # true
+print(7 <= 3)  # false
+
+print(6 >= 6)  # true
+print(4 >= 9)  # false
+
+print(8 == 8)  # true
+print(5 == 2)  # false
+
+# 2)
+# Binary code არის სისტემა, რომელიც იყენებს მხოლოდ 0-ს და 1-ს, რათა კომპიუტერმა შეასრულოს სხვადასხვა ამოცანა
